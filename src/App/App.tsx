dc38bdaff97cec6fbe01210600d03d0943c93fca
Chris Reynolds <chris.reynolds@mail.com>
2018-07-19 22:04:38 +0100

    Add slider and produce amount input to App

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,16 +1,73 @@
 import * as React from 'react';
+
+import Slider, { ESlider } from './components/Slider/Slider';
+
 import './App.scss';
 
-class App extends React.Component {
+type IState = {
+  produceAmount: string;
+  pg: string;
+  vg: string;
+};
+
+class App extends React.Component<{}, IState> {
+  state = {
+    produceAmount: '10',
+    pg: '50',
+    vg: '50',
+  };
+
+  calculateAmounts = () => {
+    const { produceAmount, pg, vg } = this.state;
+
+    return {
+      pg: (Number(pg) / 100 * Number(produceAmount)).toFixed(2),
+      vg: (Number(vg) / 100 * Number(produceAmount)).toFixed(2),
+    };
+  }
+
+  setProduceAmount = (e: React.ChangeEvent<HTMLInputElement>) => {
+    this.setState({
+      produceAmount: e.currentTarget.value,
+    });
+  }
+
+  setSliderValue = (type: ESlider) => (e: React.ChangeEvent<HTMLInputElement>) => {
+    const oppositeValue = String(100 - Number(e.currentTarget.value));
+
+    const state = type === ESlider.PG ? {
+      pg: e.currentTarget.value,
+      vg: oppositeValue,
+    } : {
+      pg: oppositeValue,
+      vg: e.currentTarget.value,
+    };
+
+    this.setState(state);
+  }
+
   render() {
+    const { calculateAmounts, state, setProduceAmount, setSliderValue } = this;
+    const { pg: calculatedPg, vg: calculatedVg } = calculateAmounts();
+
     return (
       <div className="App">
-        <div className="App-header">
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
-        </p>
+        <header className="App-header">
+          <h1>Welcome to React</h1>
+        </header>
+        <input type="number" value={state.produceAmount} onChange={setProduceAmount} />
+        <p>{state.pg}</p>
+        <Slider
+          value={state.pg}
+          onChange={setSliderValue(ESlider.PG)}
+        />
+        <p>{state.vg}</p>
+        <Slider
+          value={state.vg}
+          onChange={setSliderValue(ESlider.VG)}
+        />
+        <p>PG: {calculatedPg}ml</p>
+        <p>VG: {calculatedVg}ml</p>
       </div>
     );
   }
